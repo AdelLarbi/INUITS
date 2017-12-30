b1341666c2ea94b1c2697ce02fd0ca50b079257a
AdelLarbi <larbi.adel@yahoo.fr>
2017-12-31 00:23:29 +0100

    test fake data

--- src/fr/upmc/inuits/tests/TestPartTwo.java
+++ src/fr/upmc/inuits/tests/TestPartTwo.java
@@ -1,9 +1,20 @@
 package fr.upmc.inuits.tests;
 
 import fr.upmc.components.cvm.AbstractCVM;
+import fr.upmc.datacenter.connectors.ControlledDataConnector;
+import fr.upmc.inuits.software.autonomiccontroller.AutonomicController;
+import fr.upmc.inuits.software.requestdispatcher.RequestDispatcher;
 
 public class TestPartTwo extends AbstractCVM {
 
+	public static final String RD_DYNAMIC_STATE_DATA_IN_PORT_URI = "rddsd-ip";
+	public static final String AC_DYNAMIC_STATE_DATA_OUT_PORT_URI = "acdsd-op";
+	
+	public static final String RD_URI = "rd0";
+	
+	protected RequestDispatcher requestDispatcher;
+	protected AutonomicController autonomicController;
+	
 	public TestPartTwo() throws Exception {
 		super();
 	}
@@ -11,15 +22,44 @@ public class TestPartTwo extends AbstractCVM {
 	@Override
 	public void deploy() throws Exception {
 		
-		//TODO
+		final String[] RD0_REQUEST_SUBMISSION_OUT_PORT_URI = {"rd1rs-op", "rd2rs-op"};
+		final String[] RD0_REQUEST_NOTIFICATION_IN_PORT_URI = {"rd1rn-ip", "rd2rn-ip"};
 		
+		this.requestDispatcher = new RequestDispatcher(				
+				RD_URI,				
+				"111",
+				RD0_REQUEST_SUBMISSION_OUT_PORT_URI,
+				RD0_REQUEST_NOTIFICATION_IN_PORT_URI,
+				"222",
+				RD_DYNAMIC_STATE_DATA_IN_PORT_URI);
+		
+		this.addDeployedComponent(this.requestDispatcher);
+		
+		RequestDispatcher.DEBUG_LEVEL = 0;
+		this.requestDispatcher.toggleTracing();
+		this.requestDispatcher.toggleLogging();		
+		// --------------------------------------------------------------------
+		this.autonomicController = new AutonomicController(
+				RD_URI, 
+				AC_DYNAMIC_STATE_DATA_OUT_PORT_URI);
+		
+		this.addDeployedComponent(this.autonomicController);
+		
+		AutonomicController.DEBUG_LEVEL = 2;
+		this.autonomicController.toggleTracing();
+		this.autonomicController.toggleLogging();
+				
+		this.autonomicController.doPortConnection(
+				AC_DYNAMIC_STATE_DATA_OUT_PORT_URI,
+				RD_DYNAMIC_STATE_DATA_IN_PORT_URI,
+				ControlledDataConnector.class.getCanonicalName());
 		super.deploy();
 	}		
 	
 	@Override
 	public void shutdown() throws Exception {
 				
-		//TODO
+		this.autonomicController.doPortDisconnection(AC_DYNAMIC_STATE_DATA_OUT_PORT_URI);
 		
 		super.shutdown();
 	}
