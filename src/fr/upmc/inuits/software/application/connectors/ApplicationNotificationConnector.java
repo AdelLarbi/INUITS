4cbb910e00a437cb916acc8a648c71faa9ca88a5
MB75Z <britmohamed@hotmail.fr>
2018-02-04 22:39:43 +0100

    update

--- src/fr/upmc/inuits/software/application/connectors/ApplicationNotificationConnector.java
+++ src/fr/upmc/inuits/software/application/connectors/ApplicationNotificationConnector.java
@@ -2,11 +2,18 @@ package fr.upmc.inuits.software.application.connectors;
 
 import fr.upmc.components.connectors.AbstractConnector;
 import fr.upmc.inuits.software.application.interfaces.ApplicationNotificationI;
-
+/**
+ * la classe  <code>ApplicationNotificationConnector</code> represente un
+ * connecteur qui permet a l'application de recevoir l'information concernant sont acceptaton par l'admissionController 
+ * Permet la Connection entre ApplicationNotificationOutboundPort et ApplicationNotificationInboundPort
+ */
 public class ApplicationNotificationConnector 
 	extends AbstractConnector
 	implements ApplicationNotificationI {
 
+	/**
+	 * @see fr.upmc.inuits.software.application.interfaces.ApplicationNotificationI#notifyApplicationAdmission(boolean)
+	 */
 	@Override
 	public void notifyApplicationAdmission(boolean isAccepted) throws Exception {
 		
